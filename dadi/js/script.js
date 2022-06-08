c5d2f1b9eedb8442963568bfec4c12c63fae8c13
MayaSelena773 <maya.bono721@gmail.com>
2022-06-08 18:02:04 +0200

    dicerandom

---
+++ dadi/js/script.js
@@ -0,0 +1,15 @@
+
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+//Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+//Creazione numeri random per utente e pc
+const userNumber = Math.floor(Math.random() * 6) + 1;
+const pcNumber = Math.floor(Math.random() * 6) + 1;
+ 
+//Stampa in pagina
+document.getElementById('user-number').innerHTML = userNumber;
+document.getElementById('pc-number').innerHTML = pcNumber;
+
+//Dichiarazione vincitore
+    
