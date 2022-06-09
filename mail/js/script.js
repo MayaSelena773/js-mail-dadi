ca5dc138e253511ae20c75a11df7a0c617e85e91
MayaSelena773 <maya.bono721@gmail.com>
2022-06-09 10:30:15 +0200

    correzioniend

--- mail/js/script.js
+++ mail/js/script.js
@@ -7,21 +7,38 @@
 const mailIncluded = ['maya.selena@gmail.com','matthew.bellamy@gmail.com','chester.bennington@gmail.com','jared.leto@gmail.com','amy.winehouse@gmail.com','alicia.keys@gmail.com']
 console.log(mailIncluded)
 
-//button per accedere
+//Button per accedere
 const logInButton = document.getElementById('log-in');
 logInButton.addEventListener('click',
 function() {
 
  const userMail = document.getElementById ('user-mail').value;
+
+ //Creare la variabile flag (emailFound)
+ let emailFound = false ; 
  
  //scorri la lista delle email
  for(let i = 0; i < mailIncluded.length; i++){
 
-    //Se la mail è nella lista si apre un alert: accesso consentito
+    //const thisEmail = mailIncluded[i];
+
     if (userMail === mailIncluded[i]) {
+
+        emailFound = true ;
         
-        alert("Ciao! accesso consentito!");
      }
     }
+
+    //Se la mail è nella lista si apre un alert: accesso consentito
+    //Se la mail non è nella lista si apre un alert: accesso negato
+    if (emailFound === true) {
+
+        alert("Ciao! accesso consentito!");
+
+    }else {
+
+        alert("Accesso negato, non sei nella lista!");
+    }
+
 }
-)
+);
